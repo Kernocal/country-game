50599b32d415a0b0ecfbf52f7c9d5b951f60dcee
Sonny <sonnyelvis@hotmail.co.uk>
2022-09-06 14:49:19 +0100

    Update windi, sveltekit, svelte, vite.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-import { windi } from "svelte-windicss-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		windi({})
-	],
+	preprocess: [],
 	kit: {
 		adapter: adapter()
 	}
