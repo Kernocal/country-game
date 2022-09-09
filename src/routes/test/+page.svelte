55b2c6415c042f7c808c8a9f64c277df21e7e2bb
Sonny <sonnyelvis@hotmail.co.uk>
2022-09-09 15:07:38 +0100

    Globe projection test.

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,1 +1,11 @@
 <button class="bg-red-700">hello</button>
+
+<!-- select(baseSvg).call(drag().on("drag", (e) => {
+    const rotate = projection.rotate()
+    const k = 75 / projection.scale()
+    projection.rotate([
+        rotate[0] + e.dx * k,
+        rotate[1] - e.dy * k
+    ])
+    geoGenerator = geoPath().projection(projection);
+})); -->
